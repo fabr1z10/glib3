376835be8e54c140221434844d898d92ad8108a7
Fabrizio Venturini <fabrizioventurini@Fabrizios-MacBook-Pro.local>
2018-06-11 22:21:28 +0100

    adding path 2

--- data/smb/actions.lua
+++ data/smb/actions.lua
@@ -1,9 +1,9 @@
-function pippo(script)
+function changeRoom(roomId, script)
     table.insert (script.actions,
     {
         id = #script.actions,
  	    type = "gotoroom",
-		room = "village1"
+		room = roomId
     })
 end
 
@@ -11,11 +11,13 @@ objects = {
     stairs = {
         text = strings.objects.stairs,
         pos = {250, 0}, -- location where the player will go
-        open = pippo
+        walk = curry2(changeRoom, "village1")
     },
     path = {
         text = strings.objects.path,
-        pos = {8, 71}
+        pos = {8, 71},
+        walk = curry2(changeRoom, "lookout")
+        
     }
     
     
