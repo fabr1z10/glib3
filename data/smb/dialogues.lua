f7fe25d5a4d5dc51c552cbd348c5c95fe04f5a7b
Fabrizio Venturini <fabrizio.venturini@optrail.com>
2018-06-18 18:25:55 +0200

    adding two extra lines

--- data/smb/dialogues.lua
+++ data/smb/dialogues.lua
@@ -15,7 +15,9 @@ dialogues = {
         [1] = {
             lines = {
                 [1] = { text = strings.dialogues.lookout[1], nodenext = 2, active = true, click = curry(dialoguesLookout.start, 1) },
-                [2] = { text = strings.dialogues.lookout[2], nodenext = 2, active = true, click = curry(dialoguesLookout.start, 2) }
+                [2] = { text = strings.dialogues.lookout[2], nodenext = 2, active = true, click = curry(dialoguesLookout.start, 2) },
+                [3] = { text = strings.dialogues.lookout[3], nodenext = 2, active = true, click = curry(dialoguesLookout.start, 3) },
+                [4] = { text = strings.dialogues.lookout[4], nodenext = 2, active = true, click = curry(dialoguesLookout.start, 4) }
             }
         }
     }
