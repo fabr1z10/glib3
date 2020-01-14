0e8b83d8fd1e630c1b25675b4ffc924de47f0b1c
fabr1z10 <fabventurini@gmail.com>
2020-01-14 17:01:11 +0100

    fixed dyloader for windows

--- src/monkey/dyloader.cpp
+++ src/monkey/dyloader.cpp
@@ -23,20 +23,20 @@
         } else {
             hello(s);
         }
-}
+	}
 #elif _WIN32
     #include <windows.h>
     void DynamicLoader::load(const std::string &file, SceneFactory* s) {
         HMODULE handle = LoadLibrary(TEXT(file.c_str()));
         if (handle == NULL) {
-            GLIB_FAIL("ERROR! Cannot open extension library " << file << ": " << dlerror());
+            GLIB_FAIL("ERROR! Cannot open extension library " << file);
         } else {
             std::cout << "Loaded succesfully extension: " << file << std::endl;
         }
         typedef void (*hello_t)(SceneFactory*);
         FARPROC initializer = GetProcAddress(handle, "hello");
         if (initializer == NULL) {
-            std::cerr << "Cannot load symbol 'hello': " << dlsym_error << '\n';
+            std::cerr << "Cannot load symbol 'hello': \n";
         } else {
             hello_t hello = (hello_t) initializer;
             hello(s);
