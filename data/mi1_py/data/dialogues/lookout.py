4f5a1fbebad8466582e813a92c65c50ada8c318d
Fabrizio <fabrizio.venturini@optrail.com>
2020-09-03 22:46:07 +0200

    cia

--- data/mi1_py/data/dialogues/lookout.py
+++ data/mi1_py/data/dialogues/lookout.py
@@ -1,80 +1,80 @@
-from lib_py.scumm.dialogue import Dialogue, DialogueNode
-from lib_py.script import Script
-from lib_py.scumm.scumm import State
-from lib_py.scumm.functions import dialogueHelper2
-import lib_py.engine as engine
-import mi1_py.variables as var
-import lib_py.scumm.actions as sa
-import example
-import random
-
-# define the string array
-sl = engine.data['strings']['dialogues']['lookout']
-
-# create the dialogue
-d = Dialogue('lookout', sl)
-
-def g(n):
-    def f():
-        s = Script()
-        l = None
-        if var.talked_to_lookout>0:
-            names = [sl[53],sl[54]]
-            l = [sl[5], sl[43] + random.choice(names)]
-        else:
-            l = [sl[5], sl[6], sl[7], sl[8], sl[9]]
-        var.talked_to_lookout +=1    
-        s.addAction (sa.Say(lines = [sl[n]], tag='player'))
-        s.addAction (sa.Turn(tag='lookout', dir='s'))
-        s.addAction (sa.Say(tag='lookout', lines = l))
-        return s
-    return f
-
-def lookoutTurn(n):
-    def f():
-        s = Script()
-        s.addAction(sa.Say(tag="player", lines = [sl[n]]))
-        s.addAction(sa.Turn(tag='lookout', dir='e'))
-        s.addAction(sa.Say(tag="lookout", lines = [sl[15], sl[16]]))
-        return s
-    return f
-
-def squinky():
-    if d.nodes['imguy'].status == NodeStatus.CLOSED:
-        return dialogueHelper2(sl, ['player', 11], ['lookout', 27], ['player',28], ['lookout', 29])()        
-    else:
-        return dialogueHelper2(sl, ['player', 11], ['lookout', 41])()        
-
-def start(self):
-    d.nodes[12].active = True
-    if (not d.nodes[10].active or not d.nodes[13].active):
-        d.nodes[47].active = True
-    return
-
-def end(self):
-    s = Script()
-    s.addAction(sa.Turn(tag='lookout', dir='w'))
-    example.play(s)
-    
-d.addStartScript (start)
-d.addEndScript (end)
-
-# nodes
-d.add (1, g(1), 1, True)
-d.add (2, g(2), 1, True)
-d.add (3, g(3), 1, True)
-d.add (4, g(4), 1, True)
-
-d.add (10, dialogueHelper2 (sl, ['player', 10, 17], ['lookout', 18, 19, 20, 21]), 1, lambda: 1 if var.talked_to_lookout == 1 else 3, [10], [22], parent=[1])
-d.add (22, dialogueHelper2(sl, ['player', 22], ['lookout', 23,24,25,26]), 1, False, parent=[1])
-d.add (11, squinky, 1, lambda: 1 if var.talked_to_lookout == 1 else 3, parent=[1])
-d.add (47, dialogueHelper2(sl, ['player', 47], ['lookout', 49]), 1, False, [47], parent=[1])
-
-d.add (12, lookoutTurn(12), 1, True, closeNodes=[12], parent=[1])
-d.add (13, dialogueHelper2 (sl, ['player', 13], ['lookout', 30, 31, 32]), 1, True, [13,10,11], [34,38,47], parent=[1])
-d.add (14, dialogueHelper2 (sl, ['player', 14], ['lookout', 42]), -1, True, parent=[1],order=10)
-
-d.add (34, dialogueHelper2(sl, ['player', 34], ['lookout', 35, 36, 37]), 1, False, [34], parent=[1])
-d.add (38, dialogueHelper2(sl, ['player', 38], ['lookout', 39, 40]), 1, False, [38], parent=[1])
-
-State.addDialogue (d)
+# from lib_py.scumm.dialogue import Dialogue, DialogueNode
+# from lib_py.script import Script
+# from lib_py.scumm.scumm import State
+# from lib_py.scumm.functions import dialogueHelper2
+# import lib_py.engine as engine
+# import mi1_py.variables as var
+# import lib_py.scumm.actions as sa
+# import example
+# import random
+#
+# # define the string array
+# sl = engine.data['strings']['dialogues']['lookout']
+#
+# # create the dialogue
+# d = Dialogue('lookout', sl)
+#
+# def g(n):
+#     def f():
+#         s = Script()
+#         l = None
+#         if var.talked_to_lookout>0:
+#             names = [sl[53],sl[54]]
+#             l = [sl[5], sl[43] + random.choice(names)]
+#         else:
+#             l = [sl[5], sl[6], sl[7], sl[8], sl[9]]
+#         var.talked_to_lookout +=1
+#         s.addAction (sa.Say(lines = [sl[n]], tag='player'))
+#         s.addAction (sa.Turn(tag='lookout', dir='s'))
+#         s.addAction (sa.Say(tag='lookout', lines = l))
+#         return s
+#     return f
+#
+# def lookoutTurn(n):
+#     def f():
+#         s = Script()
+#         s.addAction(sa.Say(tag="player", lines = [sl[n]]))
+#         s.addAction(sa.Turn(tag='lookout', dir='e'))
+#         s.addAction(sa.Say(tag="lookout", lines = [sl[15], sl[16]]))
+#         return s
+#     return f
+#
+# def squinky():
+#     if d.nodes['imguy'].status == NodeStatus.CLOSED:
+#         return dialogueHelper2(sl, ['player', 11], ['lookout', 27], ['player',28], ['lookout', 29])()
+#     else:
+#         return dialogueHelper2(sl, ['player', 11], ['lookout', 41])()
+#
+# def start(self):
+#     d.nodes[12].active = True
+#     if (not d.nodes[10].active or not d.nodes[13].active):
+#         d.nodes[47].active = True
+#     return
+#
+# def end(self):
+#     s = Script()
+#     s.addAction(sa.Turn(tag='lookout', dir='w'))
+#     example.play(s)
+#
+# d.addStartScript (start)
+# d.addEndScript (end)
+#
+# # nodes
+# d.add (1, g(1), 1, True)
+# d.add (2, g(2), 1, True)
+# d.add (3, g(3), 1, True)
+# d.add (4, g(4), 1, True)
+#
+# d.add (10, dialogueHelper2 (sl, ['player', 10, 17], ['lookout', 18, 19, 20, 21]), 1, lambda: 1 if var.talked_to_lookout == 1 else 3, [10], [22], parent=[1])
+# d.add (22, dialogueHelper2(sl, ['player', 22], ['lookout', 23,24,25,26]), 1, False, parent=[1])
+# d.add (11, squinky, 1, lambda: 1 if var.talked_to_lookout == 1 else 3, parent=[1])
+# d.add (47, dialogueHelper2(sl, ['player', 47], ['lookout', 49]), 1, False, [47], parent=[1])
+#
+# d.add (12, lookoutTurn(12), 1, True, closeNodes=[12], parent=[1])
+# d.add (13, dialogueHelper2 (sl, ['player', 13], ['lookout', 30, 31, 32]), 1, True, [13,10,11], [34,38,47], parent=[1])
+# d.add (14, dialogueHelper2 (sl, ['player', 14], ['lookout', 42]), -1, True, parent=[1],order=10)
+#
+# d.add (34, dialogueHelper2(sl, ['player', 34], ['lookout', 35, 36, 37]), 1, False, [34], parent=[1])
+# d.add (38, dialogueHelper2(sl, ['player', 38], ['lookout', 39, 40]), 1, False, [38], parent=[1])
+#
+# State.addDialogue (d)
--- data/mi1_py/data/dialogues/mancomb.py
+++ data/mi1_py/data/dialogues/mancomb.py
@@ -1,56 +1,56 @@
-from lib_py.scumm.dialogue import Dialogue, DialogueNode
-from lib_py.script import Script
-from lib_py.scumm.scumm import State
-from lib_py.scumm.scripts import changeRoom
-from lib_py.scumm.functions import dialogueHelper1
-import lib_py.engine as engine
-import mi1_py.variables as var
-import lib_py.scumm.actions as sa
-import example
-
-sl = engine.data['strings']['dialogues']['mancomb']
-
-d = Dialogue('mancomb', sl)
-
-characters = [
-    ( (20,100), (255,255,255,255)),
-    ( (240,100), (255,255,255,255))
-]
-
-def end(self):
-    s =changeRoom(room = 'scummbar', pos = var.scummbar_mancomb_pos, dir='n')()
-    example.play(s)
-
-d.addEndScript (end)
-
-d.add (3, dialogueHelper1(sl, characters, [1, 3], [0, 9, 10, 11]), 3, True)
-d.add (4, dialogueHelper1(sl, characters, [1, 4], [0, 29, 30, 24, 25, 26]), 0, True, closeNodes=[4, 5], actNodes=[27])
-d.add (5, dialogueHelper1(sl, characters, [1, 5], [0, 40, 41]), 5, True, closeNodes=[4, 5], actNodes=[27])
-d.add (27, dialogueHelper1(sl, characters, [1, 27], [0, 31, 32, 33]), 27, False, closeNodes=[27])
-d.add (6, dialogueHelper1(sl, characters, [1, 6], [0, 7]), -1, True)
-
-# where's the guvnor
-d.add (34, dialogueHelper1(sl, characters, [1, 34], [0,52,53 ]), 34, True, parent=[27])
-d.add (35, dialogueHelper1(sl, characters, [1, 35], [0, 64, 65]), 0, True, parent=[27])
-d.add (36, dialogueHelper1(sl, characters, [1, 36], [0, 37, 6, 7]), -1, True, parent=[27])
-
-# why pirates not welcome?
-d.add (54, dialogueHelper1(sl, characters, [1, 54], [0, 59, 60, 61, 62, 63 ]), 0, True, parent=[34])
-d.add (55, dialogueHelper1(sl, characters, [1, 55], [0, 58, 59, 60, 61, 62, 63 ]), 0, True, parent=[34])
-d.add (56, dialogueHelper1(sl, characters, [1, 56], [0, 6, 7]), -1, True, parent=[34])
-
-# who's in charge
-d.add (42, dialogueHelper1(sl, characters, [1, 42], [0, 24, 25, 26]), 0, True, parent=[5])
-d.add (43, dialogueHelper1(sl, characters, [1, 43], [0, 31, 32, 33]), 27, True, closeNodes=[27], parent=[5])
-d.add (44, dialogueHelper1(sl, characters, [1, 44], [0, 6, 7]), -1, True, parent=[5])
-
-# my name is guybrush
-d.add (12, dialogueHelper1(sl, characters, [1, 12], [0, 16]), 3, True, closeNodes=[12], parent=[3])
-d.add (13, dialogueHelper1(sl, characters, [1, 13], [0, 17, 18]), 13, True, closeNodes=[13], parent=[3])
-d.add (14, dialogueHelper1(sl, characters, [1, 14], [0, 45, 46, 18]), 13, True, parent=[3])
-d.add (15, dialogueHelper1(sl, characters, [1, 15], [0, 47, 48]), -1, True, parent=[3])
-
-d.add (19, dialogueHelper1(sl, characters, [1, 19], [0, 23, 24, 25, 26]), 0, True, parent=[13])
-d.add (20, dialogueHelper1(sl, characters, [1, 20], [0, 28, 24, 25, 26]), 0, True, parent=[13])
-
-State.addDialogue (d)
+# from lib_py.scumm.dialogue import Dialogue, DialogueNode
+# from lib_py.script import Script
+# from lib_py.scumm.scumm import State
+# from lib_py.scumm.scripts import changeRoom
+# from lib_py.scumm.functions import dialogueHelper1
+# import lib_py.engine as engine
+# import mi1_py.variables as var
+# import lib_py.scumm.actions as sa
+# import example
+#
+# sl = engine.data['strings']['dialogues']['mancomb']
+#
+# d = Dialogue('mancomb', sl)
+#
+# characters = [
+#     ( (20,100), (255,255,255,255)),
+#     ( (240,100), (255,255,255,255))
+# ]
+#
+# def end(self):
+#     s =changeRoom(room = 'scummbar', pos = var.scummbar_mancomb_pos, dir='n')()
+#     example.play(s)
+#
+# d.addEndScript (end)
+#
+# d.add (3, dialogueHelper1(sl, characters, [1, 3], [0, 9, 10, 11]), 3, True)
+# d.add (4, dialogueHelper1(sl, characters, [1, 4], [0, 29, 30, 24, 25, 26]), 0, True, closeNodes=[4, 5], actNodes=[27])
+# d.add (5, dialogueHelper1(sl, characters, [1, 5], [0, 40, 41]), 5, True, closeNodes=[4, 5], actNodes=[27])
+# d.add (27, dialogueHelper1(sl, characters, [1, 27], [0, 31, 32, 33]), 27, False, closeNodes=[27])
+# d.add (6, dialogueHelper1(sl, characters, [1, 6], [0, 7]), -1, True)
+#
+# # where's the guvnor
+# d.add (34, dialogueHelper1(sl, characters, [1, 34], [0,52,53 ]), 34, True, parent=[27])
+# d.add (35, dialogueHelper1(sl, characters, [1, 35], [0, 64, 65]), 0, True, parent=[27])
+# d.add (36, dialogueHelper1(sl, characters, [1, 36], [0, 37, 6, 7]), -1, True, parent=[27])
+#
+# # why pirates not welcome?
+# d.add (54, dialogueHelper1(sl, characters, [1, 54], [0, 59, 60, 61, 62, 63 ]), 0, True, parent=[34])
+# d.add (55, dialogueHelper1(sl, characters, [1, 55], [0, 58, 59, 60, 61, 62, 63 ]), 0, True, parent=[34])
+# d.add (56, dialogueHelper1(sl, characters, [1, 56], [0, 6, 7]), -1, True, parent=[34])
+#
+# # who's in charge
+# d.add (42, dialogueHelper1(sl, characters, [1, 42], [0, 24, 25, 26]), 0, True, parent=[5])
+# d.add (43, dialogueHelper1(sl, characters, [1, 43], [0, 31, 32, 33]), 27, True, closeNodes=[27], parent=[5])
+# d.add (44, dialogueHelper1(sl, characters, [1, 44], [0, 6, 7]), -1, True, parent=[5])
+#
+# # my name is guybrush
+# d.add (12, dialogueHelper1(sl, characters, [1, 12], [0, 16]), 3, True, closeNodes=[12], parent=[3])
+# d.add (13, dialogueHelper1(sl, characters, [1, 13], [0, 17, 18]), 13, True, closeNodes=[13], parent=[3])
+# d.add (14, dialogueHelper1(sl, characters, [1, 14], [0, 45, 46, 18]), 13, True, parent=[3])
+# d.add (15, dialogueHelper1(sl, characters, [1, 15], [0, 47, 48]), -1, True, parent=[3])
+#
+# d.add (19, dialogueHelper1(sl, characters, [1, 19], [0, 23, 24, 25, 26]), 0, True, parent=[13])
+# d.add (20, dialogueHelper1(sl, characters, [1, 20], [0, 28, 24, 25, 26]), 0, True, parent=[13])
+#
+# State.addDialogue (d)
