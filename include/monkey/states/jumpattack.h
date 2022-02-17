e4d929d3d78f2d07198d306628ba0bad493e26fd
fabr1z10 <fabventurini@gmail.com>
2022-02-17 18:04:34 +0100

    more stuff

---
+++ include/monkey/states/jumpattack.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <monkey/states/base3d.h>
+#include <monkey/components/icollider.h>
+#include <monkey/entity.h>
+
+class JumpAttack : public Base3D {
+public:
+    JumpAttack(const ITab&);
+    void AttachStateMachine(StateMachine*) override;
+
+    void Run(double) override;
+    void Init(const ITab &) override;
+    void End() override;
+private:
+    Entity* m_target;
+    float m_peakHeight;
+    float m_downSpeed;
+    ICollisionEngine* m_engine;
+    int m_castMask;
+    int m_castTag;
+
+};
